9805008dd8eba878512e2ee5a6e71c34f23124ea
Nethravathibs123 <nethranethra451@gmail.com>
2025-01-20 03:05:20 +0530

    updated

--- public/reset.js
+++ public/reset.js
@@ -1,17 +1,17 @@
 const forgetPasswordForm = document.getElementById("forget-password-form");
 const errorMsg = document.getElementById('error');
 
+const port = 3000;
 forgetPasswordForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const email = document.getElementById('email').value;
 
     try {
-        const response = await axios.post(`http://184.73.149.88:3000/password/forgotpassword`, { email });
+        const response = await axios.post(`http://localhost:${port}/password/forgotpassword`, { email });
 
         window.location.href = "http://184.73.149.88:3000/login.html";
     }  catch(error){
         console.log('Error adding user: ',error);
-        errorMsg.textContent = "Something went wrong. Please try again later.";
     }
 
 });
